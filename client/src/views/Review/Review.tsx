07d23e0568cd97544c0770250d8b49f38fa0c47d
LegadoPrado <emmanuel941308@gmail.com>
2022-10-22 09:11:38 -0500

    trabajo sobre estilos para diseño responsivo

---
+++ client/src/views/Review/Review.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from './../../components/navbar/Navbar';
+import './../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./review.css";
+
+export const Review = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='review-container'>
+        <div className='pdf-container'>
+
+        </div>
+        <div className='com-container'>
+
+        </div>
+      </div>
+    </>
+  )
+}
